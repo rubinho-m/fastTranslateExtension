5ef6cae0a4ddee4668be67f4abcfb535a79fdb1a
Mikhail <rubinho-m@yandex.ru>
2023-09-30 11:53:46 +0300

    mvp

---
+++ script.js
@@ -0,0 +1,51 @@
+let addButton = document.getElementById('addButton')
+let englishWord = document.getElementById('englishWord')
+let russianWord = document.getElementById('russianWord')
+
+addButton.addEventListener('click', function () {
+    let dictionary = JSON.parse(localStorage.getItem('dictionary'))
+    console.log(dictionary)
+    dictionary[englishWord.value] = russianWord.value
+    localStorage.setItem('dictionary', JSON.stringify(dictionary))
+})
+
+englishWord.addEventListener('input', function () {
+    let englishValue = englishWord.value
+    let dictionary_string = localStorage.getItem('dictionary')
+    if (dictionary_string == null) {
+        localStorage.setItem('dictionary', JSON.stringify({}))
+    }
+    let dictionary = JSON.parse(localStorage.getItem('dictionary'))
+    let keys = Object.keys(dictionary)
+    let flag = false
+    let word
+    keys.forEach(str => {
+        if (word === undefined) {
+            if (str.startsWith(englishValue)) {
+                flag = true
+                word = str
+            }
+        }
+
+    })
+
+    if (flag && englishValue !== '') {
+
+        russianWord.value = word + ' - ' + dictionary[word]
+    } else {
+        russianWord.value = ''
+    }
+    checkRussianWordValue()
+
+})
+
+function checkRussianWordValue() {
+    let value = russianWord.value
+    if (value === '') {
+        addButton.classList.remove('notVisible')
+        addButton.classList.add('visible')
+    } else {
+        addButton.classList.remove('visible')
+        addButton.classList.add('notVisible')
+    }
+}

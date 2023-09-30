953316513f8b29007bf413b88de62d49ec93b971
Mikhail <rubinho-m@yandex.ru>
2023-09-30 12:44:59 +0300

    add autocomplete

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let addButton = document.getElementById('addButton')
 let englishWord = document.getElementById('englishWord')
 let russianWord = document.getElementById('russianWord')
+let datalist = document.getElementsByTagName('datalist')[0]
 
 addButton.addEventListener('click', function () {
     let dictionary = JSON.parse(localStorage.getItem('dictionary'))
@@ -19,37 +20,61 @@ englishWord.addEventListener('input', function () {
     let keys = Object.keys(dictionary)
     let flag = false
     let word
+    let autocompleteWords = []
     keys.forEach(str => {
         if (str.startsWith(englishValue)) {
+            if (str !== '') {
+                autocompleteWords.push(str)
+            }
             flag = true
-            if (word === undefined || str.length < word.length){
+            if (word === undefined || str.length < word.length) {
                 word = str
             }
         }
-
-
     })
-
+    datalist.innerHTML = ''
+    let clearFlag = false
+    autocompleteWords.forEach(autocompleteWord => {
+        let newOption = `<option value="${autocompleteWord}"></option>`
+        if (autocompleteWord !== englishValue){
+            datalist.insertAdjacentHTML('afterbegin', newOption)
+        } else {
+            clearFlag = true
+        }
+    })
+    if (clearFlag) {
+        datalist.innerHTML = ''
+    }
     if (flag && englishValue !== '') {
         russianWord.value = word + ' - ' + dictionary[word]
     } else {
         russianWord.value = ''
     }
-    checkRussianWordValue()
 
 })
+
+function selectedOption(value) {
+    let selected;
+// see if there a matching item, ignoring case
+    datalist.childNodes.forEach(function(option) {
+        if (option.value.toLowerCase() === value.toLowerCase())
+            selected = option;
+    });
+    return selected;
+}
+
+englishWord.addEventListener('keypress', function(ev) {
+    if (ev.key === 'Enter') {
+        const text = englishWord.value;
+        const selected = selectedOption(text);
+        if (selected) {
+            datalist.innerHTML = ''
+            englishWord.value = selected.value;
+            englishWord.blur();
+        }
+    }
+});
 
 russianWord.addEventListener('click', function () {
     russianWord.value = ''
 })
-
-function checkRussianWordValue() {
-    // let value = russianWord.value
-    // if (value === '') {
-    //     addButton.classList.remove('notVisible')
-    //     addButton.classList.add('visible')
-    // } else {
-    //     addButton.classList.remove('visible')
-    //     addButton.classList.add('notVisible')
-    // }
-}
